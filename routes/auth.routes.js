b3e9fea2e5738806024673a53dbed82445560c88
Max Mayer <mxhmayer@gmail.com>
2021-12-16 03:38:38 +0000

    change task and user models, condition to private pages in routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,7 +12,7 @@ const saltRounds = 10;
 router.post("/auth/signup", async (req, res, next) => {
   try {
     // Get the data from req.body
-    const { email, password, name } = req.body;
+    const { email, password, name, image } = req.body;
 
     // Validate that values are not empty strings
     if (email === "" || password === "" || name === "") {
@@ -55,6 +55,7 @@ router.post("/auth/signup", async (req, res, next) => {
       email,
       password: hashedPassword,
       name,
+      image,
     });
 
     // We should never expose passwords publicly
@@ -62,6 +63,7 @@ router.post("/auth/signup", async (req, res, next) => {
       _id: createdUser._id,
       email: createdUser.email,
       name: createdUser.name,
+      image: createdUser.image,
     };
 
     // Send the response back
